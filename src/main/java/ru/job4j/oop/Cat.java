a5cadc07a127e22af6f8c02000cbcf5fcf638e06
rinat <rinatlatypov94@gmail.com>
2021-02-27 12:19:09 +0300

    1.5. Поля объекта [#386999]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -6,11 +6,11 @@ public class Cat {
     private String name;
 
     public void giveNick(String nick){
-
+        this.name = nick;
     }
 
     public void show() {
-        System.out.println(this.food);
+        System.out.println(this.name + this.food);
     }
 
     public void eat(String meat) {
@@ -18,15 +18,14 @@ public class Cat {
     }
 
     public static void main(String[] args) {
-        System.out.println("There are gav's food.");
         Cat gav = new Cat();
         gav.eat("kotleta");
+        gav.giveNick("gav ");
         gav.show();
         gav.eat("kotleta");
-        System.out.println("There are black's food.");
         Cat black = new Cat();
         black.eat("fish");
+        black.giveNick("black ");
         black.show();
-
     }
 }
