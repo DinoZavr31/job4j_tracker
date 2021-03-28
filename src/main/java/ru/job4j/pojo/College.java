2b9d33e987604844e1f787aff216c1b6880ad2a5
rinat <rinatlatypov94@gmail.com>
2021-03-28 12:16:58 +0300

    2. Модель данных. [#387019]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setLastName("Latypov ");
+        student.setFirstName("Rinat ");
+        student.setMiddleName("Nailevich ");
+        student.setGroup("job4j");
+        student.setCreated(new Date());
+        System.out.println(student.getLastName() + student.getFirstName() + student.getMiddleName() + ": group - " + student.getGroup() + ", date - " + student.getCreated());
+
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,50 @@
+package ru.job4j.pojo;
+import java.util.Date;
+
+public class Student {
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private String group;
+    private Date created;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
